8424bbbd3d2cbfe8fca77d627bed85e7bf484f03
Hedie Hamdamian <165987346+Hediehamdam@users.noreply.github.com>
2026-02-09 21:26:17 +0330

    Update sudoku_solver.py

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,1 +1,61 @@
 
+"""
+File Name: sudoku_solver.py
+Description: Solves 9x9 Sudoku using Backtracking algorithm.
+Data Structure/Algorithm: 9x9 Matrix, Backtracking
+Operations: solve, is_valid
+Use Case: Automatic Sudoku solving, practice backtracking
+"""
+
+def print_board(board):
+    for row in board:
+        print(row)
+    print()
+
+def is_valid(board, row, col, num):
+    # Check row
+    for i in range(9):
+        if board[row][i] == num:
+            return False
+    # Check column
+    for i in range(9):
+        if board[i][col] == num:
+            return False
+    # Check 3x3 subgrid
+    start_row = row - row % 3
+    start_col = col - col % 3
+    for i in range(3):
+        for j in range(3):
+            if board[start_row + i][start_col + j] == num:
+                return False
+    return True
+
+def solve_sudoku(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, row, col, num):
+                        board[row][col] = num
+                        if solve_sudoku(board):
+                            return True
+                        board[row][col] = 0
+                return False
+    return True
+
+if __name__ == "__main__":
+    sudoku_board = [
+        [5,3,0,0,7,0,0,0,0],
+        [6,0,0,1,9,5,0,0,0],
+        [0,9,8,0,0,0,0,6,0],
+        [8,0,0,0,6,0,0,0,3],
+        [4,0,0,8,0,3,0,0,1],
+        [7,0,0,0,2,0,0,0,6],
+        [0,6,0,0,0,0,2,8,0],
+        [0,0,0,4,1,9,0,0,5],
+        [0,0,0,0,8,0,0,7,9]
+    ]
+    if solve_sudoku(sudoku_board):
+        print_board(sudoku_board)
+    else:
+        print("No solution exists.")
